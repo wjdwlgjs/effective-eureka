c0a2407febfa7a84dd8c481858a937a7470816ab
unknown <love4rg@gmail.com>
2022-07-28 14:08:06 +0900

    .

--- 16496.py
+++ 16496.py
@@ -4,12 +4,14 @@ from functools import cmp_to_key
 n = int(stdin.readline())
 input_list = stdin.readline().split()
 
-def compare(a:str, b:str):
-    if int(b + a) > int(a + b):
-        return 1
-    else:
-        return -1
+def list_to_int(_list: list):
+    return_str = ""
+    for element in _list:
+        return_str += element
+    return int(return_str)
 
-input_list.sort(key = cmp_to_key(compare))
-for i in input_list:
-    print(i, end = "")
+def compare(a:str, b:str):
+    return int(b + a) - int(a + b)
+
+input_list.sort(key=cmp_to_key(compare))
+print(list_to_int(input_list))
